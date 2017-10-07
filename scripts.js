12518d863172957830fd08ddc68fa896d655f0a2
Sean Coley <seanwcoley@gmail.com>
2017-10-07 18:29:40 +1300

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$("#clientForm").on("submit", function(){
+		writeForm(e)
+		alert("form submit function triggered successfully")
+	});
+
+	function writeForm (e) {
+
+	}
+
+});
