db350e0e280e874d9df02b332fc757d283593916
Sean Coley <seanwcoley@gmail.com>
2017-10-16 21:25:39 +1300

    HTML + script working on refresh

--- scripts.js
+++ scripts.js
@@ -1,12 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$("#clientForm").on("submit", function(){
-		writeForm(e)
-		alert("form submit function triggered successfully")
-	});
+    $("#clientForm").on("submit", function(e) {
 
-	function writeForm (e) {
+        console.log(clientForm);
+        writeForm(e);
+        e.preventDefault();
 
-	}
+    });
+
+    function writeForm (e) {
+
+    	var accountName = $("#accountName").val();
+		var mainContactFirstName = $("#mainContactFirstName").val();
+		var mainContactLastName = $("#mainContactLastName").val();
+		var mainContactEmail = $("#mainContactEmail").val();
+		var mainContactLandline = $("#mainContactLandline").val();
+		var mainContactMobile = $("#mainContactMobile").val();
+		var businessName = $("#businessName").val();
+		var billingStreetAddress = $("#billingStreetAddress").val();
+		var billingAddressSuburb = $("#billingAddressSuburb").val();
+		var territory = $("#territory").val();
+		var customerDemographic = $("#customerDemographic").val();
+		var estimatedCustomerIncome = $("#estimatedCustomerIncome").val();
+		var acquisitionChannel = $("#acquisitionChannel").val();
+
+    	console.log(accountName)
+
+    }
 
 });
