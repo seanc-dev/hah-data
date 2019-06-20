bdef1b8b3c9f5b7bd9fdef5325ff972e3e594d89
Sean Coley <seanwcoley@gmail.com>
2019-06-20 18:11:37 +0300

    trying credentials env var with escaped linebreaks

--- preinstall.js
+++ preinstall.js
@@ -2,6 +2,6 @@ const   fs = require("fs");
 
 fs.writeFile("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG, (err) => {});
 
-console.log("preinstall completed successfully with credentials:");
-
 console.log(require("./google-credentials-heroku.json"));
+
+console.log("preinstall completed successfully with credentials:");
