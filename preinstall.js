8de6e44cdf422d0c12bfc9a21ca93b80508d817c
Sean Coley <seanwcoley@gmail.com>
2019-06-20 18:00:21 +0300

    retry preinstall file creation

--- preinstall.js
+++ preinstall.js
@@ -1,20 +1,7 @@
-// const   fs = require("fs");
+const   fs = require("fs");
 
-// fs.writeFile("./google-credentials-heroku.json", process.env.GOOGLE_CONFIG, (err) => {});
+fs.writeFile("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG, (err) => {});
 
-// console.log("preinstall completed successfully with credentials:");
+console.log("preinstall completed successfully with credentials:");
 
-// fs.readFile("./google-credentials-heroku.json", function(file){
-//     console.log(file);
-// });
-
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-
-    console.log("NODE_ENV === development");
-
-} else {
-
-    console.log("NODE_ENV !== development");
-
-}
+console.log(require("./google-credentials-heroku.json"));
