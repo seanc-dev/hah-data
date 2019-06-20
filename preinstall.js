832d3be1c0bc0d2bbe456338397ec1fc34a93876
Sean Coley <seanwcoley@gmail.com>
2019-06-20 18:17:09 +0300

    add fs.readFile to preinstall.js

--- preinstall.js
+++ preinstall.js
@@ -2,6 +2,12 @@ const   fs = require("fs");
 
 fs.writeFile("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG, (err) => {});
 
+fs.readFile("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, function(file){
+    console.log("preinstall fs.readFile callback credentials file");
+    console.log(file);
+});
+
 console.log(require("./google-credentials-heroku.json"));
 
 console.log("preinstall completed successfully with credentials:");
+
