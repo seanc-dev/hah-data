78f026d95948266481e8909f3d820b5e50e64a0f
Sean Coley <seanwcoley@gmail.com>
2019-06-20 16:26:21 +0300

    when will it end

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const   bodyParser      = require('body-parser'),
         express         = require('express'),
         ejs             = require("ejs");
 
-if (process.env.NODE_ENV === "development") const dotenv = require("dotenv")
-if (process.env.NODE_ENV === "development") dotenv.config();
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    
+    const   dotenv = require("dotenv");
+    dotenv.config();
+
+}
 
 const   app = express();
 
