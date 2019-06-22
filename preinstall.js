8091678edc416aab37350247cea02e610ff86e45
Sean Coley <seanwcoley@gmail.com>
2019-06-22 15:48:34 +0300

    try with fs.writeFileSync

--- preinstall.js
+++ preinstall.js
@@ -1,10 +1,4 @@
 const   fs = require("fs");
 
-fs.writeFile("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG, (err) => {
-
-    if (err) console.error(err);
-    
-    console.log("preinstall completed successfully with credentials:");
-
-});
+fs.writeFileSync("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG);
 
