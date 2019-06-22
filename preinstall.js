aef5ad48c4d5e35b05d6e92dbfadcd5fc999820e
Sean Coley <seanwcoley@gmail.com>
2019-06-22 17:30:44 +0300

    check preinstall created file

--- preinstall.js
+++ preinstall.js
@@ -1,18 +1,5 @@
 const   fs = require("fs");
 
-fs.writeFile("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG, (err) => {
+fs.writeFileSync("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG);
 
-    if (err) console.error(err);
-    
-    console.log("preinstall completed successfully with credentials:");
-
-    fs.readFile("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, function(err, file){
-
-        if (err) console.error(err);
-
-        console.log(file);
-
-    })
-
-});
-
+console.log(require("./" + process.env.GOOGLE_CREDENTIALS_LOCATION));
