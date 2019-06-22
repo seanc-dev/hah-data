89e31dae56b93d54221b639ee433fd4b146357c8
Sean Coley <seanwcoley@gmail.com>
2019-06-22 15:51:09 +0300

    fix spreadsheet creds error

--- preinstall.js
+++ preinstall.js
@@ -1,4 +1,18 @@
 const   fs = require("fs");
 
-fs.writeFileSync("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG);
+fs.writeFile("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG, (err) => {
+
+    if (err) console.error(err);
+    
+    console.log("preinstall completed successfully with credentials:");
+
+    fs.readFile("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, function(err, file){
+
+        if (err) console.error(err);
+
+        console.log(file);
+
+    })
+
+});
 
