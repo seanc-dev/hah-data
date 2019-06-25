a3e33358de1152c910ffdb21f9d810fce86c4d7c
Sean Coley <seanwcoley@gmail.com>
2019-06-25 13:14:19 +0400

    push live production sheets

--- preinstall.js
+++ preinstall.js
@@ -1,5 +1,3 @@
 const   fs = require("fs");
 
 fs.writeFileSync("./" + process.env.GOOGLE_CREDENTIALS_LOCATION, process.env.GOOGLE_CONFIG);
-
-console.log(require("./lib/config.js")[process.env.NODE_ENV]);
