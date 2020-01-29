c5144c70cdd0f8148400f7160320eb9b2607c720
Sean Coley <seanwcoley@gmail.com>
2020-01-29 20:06:14 +0100

    clean up routes logic

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,139 +1,136 @@
 const express = require("express");
 
 const Job = require("../lib/classes/job.js"),
-    lib = require("../lib/library.js"),
-    geocode = require("./services/geocode.js"),
-    ss = require("../lib/spreadsheet.js"),
-    getData = require("./services/getData.js");
+	lib = require("../lib/library.js"),
+	geocode = require("./services/geocode.js"),
+	ss = require("../lib/spreadsheet.js"),
+	getData = require("./services/getData.js");
 
 const config = require("../lib/config.js");
 
 const router = express.Router({
-    mergeParams: true
+	mergeParams: true,
 });
 
 // index route
 router.get("/", (req, res) => {
-    if (req.query.requestType === "detailsArray") {
-        getData.getJobDetailsArray(req)
-            .then((result) => {
-                res.send(result);
-            })
-            .catch((err) => {
-                console.error("Failed to retrieve job details array");
-                console.error(err);
-                res.status(500).send(err);
-            });
-    } else if (req.query.requestType === 'keys') {
-        let job = new Job(req.params.orgId, 1, false);
-        getData.getKeys(job, req, res)
-            .then((result) => {
-                res.send(result);
-            })
-            .catch((err) => {
-                console.error(err);
-                res.status(500).send(err);
-            });
-    }
+	if (req.query.requestType === "detailsArray") {
+		getData
+			.getJobDetailsArray(req)
+			.then(result => {
+				res.send(result);
+			})
+			.catch(err => {
+				console.error("Failed to retrieve job details array");
+				console.error(err);
+				res.status(500).send(err);
+			});
+	} else if (req.query.requestType === "keys") {
+		let job = new Job(req.params.orgId, 1, false);
+		getData
+			.getKeys(job, req, res)
+			.then(result => {
+				res.send(result);
+			})
+			.catch(err => {
+				console.error(err);
+				res.status(500).send(err);
+			});
+	}
 });
 
 // create route
 router.post("/", (req, res) => {
+	let location = `${req.body.workLocationStreet} 
+        ${req.body.workLocationSuburb} 
+        ${req.body.workLocationCity} 
+        ${req.body.workLocationPostcode}`;
 
-    let location = req.body.workLocationStreet + " " + req.body.workLocationSuburb + " " + req.body.workLocationCity + " " + req.body.workLocationPostcode
+	geocode(location)
+		.then(function(result) {
+			req.body.workLocationLatitude = result[0].latitude;
+			req.body.workLocationLongitude = result[0].longitude;
+			req.body.workLocationFormattedAddress = result[0].formattedAddress;
+			req.body.workLocationGPID = result[0].extra.googlePlaceId;
 
-    geocode(location)
-        .then(function (result) {
+			getData.crud(
+				res,
+				new Job(req.params.orgId, false, req.body),
+				"new",
+			);
+		})
+		.catch(err => {
+			console.error(
+				"Failed to geocode Billing Address for job " + req.body.jobId,
+			);
+			console.error(err);
 
-            req.body.workLocationLatitude = result[0].latitude
-            req.body.workLocationLongitude = result[0].longitude
-            req.body.workLocationFormattedAddress = result[0].formattedAddress
-            req.body.workLocationGPID = result[0].extra.googlePlaceId
-
-            let job = new Job(req.params.orgId, false, req.body);
-            getData.dataObjInit(job, "new", "create")
-                .then((result) => {
-                    console.log('job create route getData.dataObjInit(job, "new", "create") result:')
-                    console.log(result);
-                    console.log("Successfully created job record for jobid " + result.id)
-                    res.send(result);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    res.status(500).send(err);
-                });
-
-        })
-        .catch((err) => {
-
-            console.error("Failed to geocode Billing Address for job " + req.body.jobId);
-            console.error(err);
-
-            let job = new Job(req.params.orgId, false, req.body);
-            getData.dataObjInit(job, "new", "create")
-                .then((result) => {
-                    console.log('job create route getData.dataObjInit(job, "new", "create") result:')
-                    console.log(result);
-                    console.log("Successfully created job record for jobid " + result.id)
-                    res.send(result);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    res.status(500).send(err);
-                });
-
-        });
-
+			getData.crud(
+				res,
+				new Job(req.params.orgId, false, req.body),
+				"new",
+			);
+		});
 });
 
 // show
 router.get("/:id", (req, res) => {
+	getData.crud(res, new Job(req.params.orgId, req.params.id, false), "view");
 
-    let job = new Job(req.params.orgId, req.params.id, false);
-    getData.dataObjInit(job, "view", "show")
-        .then((result) => {
-            console.log("Retrieved details for job with id " + req.params.id);
-            res.send(result);
-        })
-        .catch((err) => {
-            console.error(err);
-            console.error("Error in jobs show route")
-            res.status(500).send(err);
-        });
-
+	// let job = new Job(req.params.orgId, req.params.id, false);
+	// getData
+	// 	.dataObjInit(job, "view", "show")
+	// 	.then(result => {
+	// 		console.log("Retrieved details for job with id " + req.params.id);
+	// 		res.send(result);
+	// 	})
+	// 	.catch(err => {
+	// 		console.error(err);
+	// 		console.error("Error in jobs show route");
+	// 		res.status(500).send(err);
+	// 	});
 });
 
 // update
 router.put("/:id", (req, res) => {
-    let job = new Job(req.params.orgId, req.params.id, req.body);
-    getData.dataObjInit(job, "edit", "update")
-        .then((result) => {
-            console.log("jobs update route init func result:")
-            console.log(result)
-            console.log("Updated details for job with id " + req.params.id);
-            res.send(result);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send(err);
-        });
+	getData.crud(
+		res,
+		new Job(req.params.orgId, req.params.id, req.body),
+		"edit",
+	);
 
+	// let job = new Job(req.params.orgId, req.params.id, req.body);
+	// getData
+	// 	.dataObjInit(job, "edit", "update")
+	// 	.then(result => {
+	// 		console.log("Updated details for job with id " + req.params.id);
+	// 		res.send(result);
+	// 	})
+	// 	.catch(err => {
+	// 		console.error(err);
+	// 		res.status(500).send(err);
+	// 	});
 });
 
 // destroy
 router.delete("/:id", (req, res) => {
+	getData.crud(
+		res,
+		new Job(req.params.orgId, req.params.id, req.body),
+		"delete",
+	);
 
-    let job = new Job(req.params.orgId, req.params.id, req.body);
-    getData.dataObjInit(job, "delete", "destroy")
-        .then((result) => {
-            console.log("Deleted details for job with id " + req.params.id);
-            res.send(result);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send(err);
-        });
-
+	// let job = new Job(req.params.orgId, req.params.id, req.body);
+	// getData
+	// 	.dataObjInit(job, "delete", "destroy")
+	// 	.then(result => {
+	// 		console.log("Deleted details for job with id " + req.params.id);
+	// 		res.send(result);
+	// 	})
+	// 	.catch(err => {
+	// 		console.error(err);
+	// 		res.status(500).send(err);
+	// 	});
 });
 
 module.exports = router;
--- routes/services/getData.js
+++ routes/services/getData.js
@@ -1,71 +1,102 @@
 const lib = require("../../lib/library.js"),
-    ss = require("../../lib/spreadsheet.js"),
-    mapping = require("../../lib/mapping.js");
+	ss = require("../../lib/spreadsheet.js"),
+	mapping = require("../../lib/mapping.js");
 
 module.exports = {
+	crud: function(res, inst, requestType) {
+		// initialise instance with requested action
+		let init = new Promise((resolve, reject) => {
+			try {
+				resolve(inst.init(requestType));
+			} catch (err) {
+				console.error(
+					`Error in ${inst.dimension} ${requestType} crud service: Failed to initiate instance`,
+				);
+				console.error(err);
+				reject(err);
+			}
+		});
 
-    dataObjInit: async function (instance, requestType, routeName) {
+		init.then(result => {
+			console.log(`init result`);
+			console.log(result);
+			console.log(
+				`Successful ${requestType} init for ${
+					inst.dimension
+				} record with id ${
+					result[`${inst.dimension.toLowerCase()}Id`]
+				}`,
+			);
+			res.send(result);
+		}).catch(err => {
+			console.error(err);
+			res.status(500).send(err);
+		});
+	},
 
-        console.log(`dataObjInit instance dim = ${instance.dimension}, requestType = ${requestType}`);
+	dataObjInit: async function(inst, requestType, routeName) {
+		console.log(
+			`dataObjInit instance dim = ${inst.dimension}, requestType = ${requestType}`,
+		);
 
-        return new Promise((resolve, reject) => {
-            try {
-                resolve(instance.init(requestType))
-            } catch (err) {
-                console.error(`Error in ${instance.dimension} ${requestType} dataObjInit service: Failed to initiate instance`);
-                console.error(err);
-                reject(err);
-            }
-        });
+		return new Promise((resolve, reject) => {
+			try {
+				resolve(inst.init(requestType));
+			} catch (err) {
+				console.error(
+					`Error in ${inst.dimension} ${requestType} crud service: Failed to initiate instance`,
+				);
+				console.error(err);
+				reject(err);
+			}
+		});
+	},
 
-    },
+	getAddressString: async function(req) {
+		return await ss.getAddressDetailsString(
+			req.params.orgId,
+			req.query.clientId,
+		);
+	},
 
-    getAddressString: async function (req) {
-        return await ss.getAddressDetailsString(req.params.orgId, req.query.clientId)
-    },
+	getClientDetailsArray: async function(req) {
+		return await ss.getClientDetailsArray(req.params.orgId);
+	},
 
-    getClientDetailsArray: async function (req) {
+	getJobDetailsArray: async function(req) {
+		return await ss.getJobDetailsArray(req.params.orgId);
+	},
 
-        return await ss.getClientDetailsArray(req.params.orgId);
+	getKeys: async function(dataObjInst, req, res) {
+		let initResult;
+		try {
+			// call Dataobject.init to return data row
+			initResult = await dataObjInst.init("view");
+		} catch (err) {
+			console.error(
+				"Failed to retrieve db record in " +
+					dataObjInst.dimension +
+					"s index route: keys",
+			);
+			console.error(err);
+			return err;
+		}
 
-    },
+		// get keys for client record (fieldnames)
+		let keysArr = Object.keys(initResult);
+		let fieldLabels = [];
 
-    getJobDetailsArray: async function (req) {
+		// map in labels from fieldnames and send to client
+		for (i = 0; i < keysArr.length; i++) {
+			fieldLabels[i] = lib.getFieldLabelFromName(
+				mapping[req.params.orgId][dataObjInst.dimension],
+				keysArr[i],
+			);
+		}
 
-        return await ss.getJobDetailsArray(req.params.orgId);
-
-    },
-
-    getKeys: async function (dataObjInst, req, res) {
-
-        let initResult;
-        try {
-
-            // call Dataobject.init to return data row
-            initResult = await dataObjInst.init("view");
-
-        } catch (err) {
-
-            console.error("Failed to retrieve db record in " + dataObjInst.dimension + "s index route: keys");
-            console.error(err);
-            return err;
-
-        }
-
-        // get keys for client record (fieldnames)
-        let keysArr = Object.keys(initResult);
-        let fieldLabels = [];
-
-        // map in labels from fieldnames and send to client
-        for (i = 0; i < keysArr.length; i++) {
-            fieldLabels[i] = lib.getFieldLabelFromName(mapping[req.params.orgId][dataObjInst.dimension], keysArr[i]);
-        }
-
-        return {
-            fieldLabels: fieldLabels,
-            fieldNames: keysArr
-        };
-
-    }
-
-}
+		return {
+			fieldLabels: fieldLabels,
+			fieldNames: keysArr,
+		};
+	},
+};
