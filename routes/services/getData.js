d7e4f8b374c2bd35551df0c944f63d0e67972473
Sean Coley <seanwcoley@gmail.com>
2020-06-03 15:10:42 +0200

    Commas and update res.status(status) to res.sendStatus(status) as per express update

--- routes/services/getData.js
+++ routes/services/getData.js
@@ -1,103 +1,103 @@
 const lib = require("../../lib/library.js"),
-	ss = require("../../lib/spreadsheet.js"),
-	mapping = require("../../lib/mapping.js");
+  ss = require("../../lib/spreadsheet.js"),
+  mapping = require("../../lib/mapping.js");
 
 module.exports = {
-	crud: function(res, inst, requestType) {
-		// initialise instance with requested action
-		let init = new Promise((resolve, reject) => {
-			try {
-				resolve(inst.init(requestType));
-			} catch (err) {
-				console.error(
-					`Error in ${inst.dimension} ${requestType} crud service: Failed to initiate instance`,
-				);
-				console.error(err);
-				reject(err);
-			}
-		});
+  crud: function (res, inst, requestType) {
+    // initialise instance with requested action
+    let init = new Promise((resolve, reject) => {
+      try {
+        resolve(inst.init(requestType));
+      } catch (err) {
+        console.error(
+          `Error in ${inst.dimension} ${requestType} crud service: Failed to initiate instance`
+        );
+        console.error(err);
+        reject(err);
+      }
+    });
 
-		init.then(result => {
-			console.log(
-				`Successful ${requestType} init for ${
-					inst.dimension
-				} record with id ${
-					result[`${inst.dimension.toLowerCase()}Id`]
-				}`,
-			);
-			res.send(result);
-			if (inst.dimension === "job" && requestType !== "view") {
-				inst.initClientUpdate.bind(inst)();
-			}
-		}).catch(err => {
-			console.error(err);
-			res.status(500).send(err);
-		});
-	},
+    init
+      .then((result) => {
+        console.log(
+          `Successful ${requestType} init for ${
+            inst.dimension
+          } record with id ${result[`${inst.dimension.toLowerCase()}Id`]}`
+        );
+        res.send(result);
+        if (inst.dimension === "job" && requestType !== "view") {
+          inst.initClientUpdate.bind(inst)();
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500).send(err);
+      });
+  },
 
-	// dataObjInit: async function(inst, requestType, routeName) {
-	// 	console.log(
-	// 		`dataObjInit instance dim = ${inst.dimension}, requestType = ${requestType}`,
-	// 	);
+  // dataObjInit: async function(inst, requestType, routeName) {
+  // 	console.log(
+  // 		`dataObjInit instance dim = ${inst.dimension}, requestType = ${requestType}`,
+  // 	);
 
-	// 	return new Promise((resolve, reject) => {
-	// 		try {
-	// 			resolve(inst.init(requestType));
-	// 		} catch (err) {
-	// 			console.error(
-	// 				`Error in ${inst.dimension} ${requestType} crud service: Failed to initiate instance`,
-	// 			);
-	// 			console.error(err);
-	// 			reject(err);
-	// 		}
-	// 	});
-	// },
+  // 	return new Promise((resolve, reject) => {
+  // 		try {
+  // 			resolve(inst.init(requestType));
+  // 		} catch (err) {
+  // 			console.error(
+  // 				`Error in ${inst.dimension} ${requestType} crud service: Failed to initiate instance`,
+  // 			);
+  // 			console.error(err);
+  // 			reject(err);
+  // 		}
+  // 	});
+  // },
 
-	getAddressString: async function(req) {
-		return await ss.getAddressDetailsString(
-			req.params.orgId,
-			req.query.clientId,
-		);
-	},
+  getAddressString: async function (req) {
+    return await ss.getAddressDetailsString(
+      req.params.orgId,
+      req.query.clientId
+    );
+  },
 
-	getClientDetailsArray: async function(req) {
-		return await ss.getClientDetailsArray(req.params.orgId);
-	},
+  getClientDetailsArray: async function (req) {
+    return await ss.getClientDetailsArray(req.params.orgId);
+  },
 
-	getJobDetailsArray: async function(req) {
-		return await ss.getJobDetailsArray(req.params.orgId);
-	},
+  getJobDetailsArray: async function (req) {
+    return await ss.getJobDetailsArray(req.params.orgId);
+  },
 
-	getKeys: async function(dataObjInst, req, res) {
-		let initResult;
-		try {
-			// call Dataobject.init to return data row
-			initResult = await dataObjInst.init("view");
-		} catch (err) {
-			console.error(
-				"Failed to retrieve db record in " +
-					dataObjInst.dimension +
-					"s index route: keys",
-			);
-			console.error(err);
-			return err;
-		}
+  getKeys: async function (dataObjInst, req, res) {
+    let initResult;
+    try {
+      // call Dataobject.init to return data row
+      initResult = await dataObjInst.init("view");
+    } catch (err) {
+      console.error(
+        "Failed to retrieve db record in " +
+          dataObjInst.dimension +
+          "s index route: keys"
+      );
+      console.error(err);
+      return err;
+    }
 
-		// get keys for client record (fieldnames)
-		let keysArr = Object.keys(initResult);
-		let fieldLabels = [];
+    // get keys for client record (fieldnames)
+    let keysArr = Object.keys(initResult);
+    let fieldLabels = [];
 
-		// map in labels from fieldnames and send to client
-		for (i = 0; i < keysArr.length; i++) {
-			fieldLabels[i] = lib.getFieldLabelFromName(
-				mapping[req.params.orgId][dataObjInst.dimension],
-				keysArr[i],
-			);
-		}
+    // map in labels from fieldnames and send to client
+    for (i = 0; i < keysArr.length; i++) {
+      fieldLabels[i] = lib.getFieldLabelFromName(
+        mapping[req.params.orgId][dataObjInst.dimension],
+        keysArr[i]
+      );
+    }
 
-		return {
-			fieldLabels: fieldLabels,
-			fieldNames: keysArr,
-		};
-	},
+    return {
+      fieldLabels: fieldLabels,
+      fieldNames: keysArr,
+    };
+  },
 };
