a3337bc7057852339f94282faa2b847b84297876
Sean Coley <seanwcoley@gmail.com>
2020-06-12 19:51:31 +0200

    Add db queries for client index route

---
+++ routes/services/queries/clients.js
@@ -0,0 +1,44 @@
+const { pool } = require("./../../../lib/db_config");
+
+module.exports = {
+  getClientDetails: (req, res) => {
+    pool.query(
+      "SELECT c.id, accountname, billingaddressstreet, billingaddresssuburb FROM client as c inner join organisation as o on c.organisationid = o.id where o.shortname = $1",
+      [req.params.orgId],
+      (error, results) => {
+        if (error) {
+          console.error(error);
+          throw error;
+        }
+        console.log("getClientDetails results.length: " + results.rows.length);
+        // res.status(200).json(results.rows);
+      }
+    );
+  },
+  getAddressDetailsById: (req, res, clientId) => {
+    pool.query(
+      "SELECT c.billingaddressstreet, c.billingaddresssuburb FROM client as c inner join organisation as o on c.organisationid = o.id where o.shortname = $1 and c.id = $2",
+      [req.params.orgId, clientId],
+      (error, results) => {
+        if (error) {
+          console.error(error);
+          throw error;
+        }
+        console.log("getAddressDetailsById results");
+        console.log("results.length: " + results.rows.length);
+        console.log(results.rows);
+        // res.status(200).json(results.rows);
+      }
+    );
+  },
+  getColumnHeaders: (tableName) => {
+    pool.query(
+      "select column_name from information_schema.columns where table_name = $1",
+      [tableName],
+      (error, results) => {
+        if (error) throw error;
+        console.log(results.rows);
+      }
+    );
+  },
+};
