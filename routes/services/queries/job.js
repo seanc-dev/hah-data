c9e5e38dff98cd2c0bbf15ea13fcc8a0ae916f38
Sean Coley <seanwcoley@gmail.com>
2020-06-14 18:25:24 +0200

    Add getJobDetails func

--- routes/services/queries/job.js
+++ routes/services/queries/job.js
@@ -1,3 +1,27 @@
 const { pool } = require("./../../../lib/db_config");
 
-module.exports = {};
+module.exports = {
+  getJobDetails: (req, res) => {
+    pool.query(
+      "SELECT c.id as clientId, j.id as jobId, c.accountname, j.dateinvoicesentutc, j.amountinvoiced FROM job as j inner join (select id, accountname, organisationid from client) as c on j.clientid = c.id inner join organisation as o on c.organisationid = o.id where o.shortname = $1",
+      [req.params.orgId],
+      (error, result) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send(error);
+        }
+        console.log("getClientDetails result.length: " + result.rows.length);
+        let arr = result.rows.map((obj) => {
+          return {
+            clientId: obj.clientid,
+            jobId: obj.jobid,
+            accountName: obj.accountname,
+            dateInvoiceSent: obj.dateinvoicesentutc,
+            amountInvoiced: obj.amountinvoiced,
+          };
+        });
+        res.send(arr);
+      }
+    );
+  },
+};
