526b196c7f784669b9fbdb7d70763d5ff8576011
Sean Coley <seanwcoley@gmail.com>
2020-06-14 18:25:07 +0200

    Correct getColumnHeaders query and add getStaffNames

--- routes/services/queries/index.js
+++ routes/services/queries/index.js
@@ -1,16 +1,31 @@
 const { pool } = require("./../../../lib/db_config");
 
 module.exports = {
+  client: require("./client"),
+  job: require("./job"),
   getColumnHeaders: async (tableName) => {
     let result;
     try {
       result = await pool.query(
-        "select column_name from information_schema.columns where table_name = $1",
+        "select column_name from information_schema.columns where table_name = $1 and column_name != 'organisationid'",
         [tableName]
       );
     } catch (err) {
-      throw err;
+      console.error(err);
     }
+    console.log(result.rows);
     return result.rows;
+  },
+  getStaffNames: async (orgShortName) => {
+    let result;
+    try {
+      result = await pool.query(
+        "select distinct staffmembername from staff as s inner join organisation as o on s.organisationid = o.id where o.shortname = $1",
+        [orgShortName]
+      );
+    } catch (err) {
+      console.error(err);
+    }
+    return results.rows.map((row) => row.staffmembername);
   },
 };
