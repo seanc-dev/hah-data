2b12a03cb1747348c61146a463354cee763ac1f2
Sean Coley <seanwcoley@gmail.com>
2020-06-16 17:33:53 +0200

    Remove fucktonnes of redundant code

--- routes/clients.js
+++ routes/clients.js
@@ -1,13 +1,6 @@
 const express = require("express");
 
-const lib = require("../lib/library.js"),
-  ss = require("../lib/spreadsheet"),
-  Client = require("../lib/classes/client.js"),
-  // geocodeAddress = require("./services/geocode.js"),
-  getData = require("./services/getData.js"),
-  queries = require("./services/queries/client");
-
-const config = require("../lib/config.js");
+const queries = require("./services/queries/client");
 const { getKeysFromDb } = require("./services/getData.js");
 
 const router = express.Router({
@@ -24,64 +17,15 @@ router.get("/", (req, res) => {
 });
 
 // create route
-router.post("/", (req, res) => {
-  ss.getClientDetailsArray(req.params.orgId)
-    .then((result) => {
-      // test for account name similarity
-
-      if (
-        result.find((el) => {
-          el.accountName === req.body.accountName;
-        })
-      ) {
-        let errorMessage =
-          "Client record with that account name already exists in database";
-        console.error(errorMessage);
-        res.status(500).send(new Error(errorMessage));
-      }
-    })
-    // .then((result) => {this is where I would add the test for address similarity if requested (note - should be replaced by db keys later)})
-    .catch((error) => {
-      console.error(error);
-      console.error(
-        "Error in client create route: Failed to get client details array from DB"
-      );
-    })
-    .then((result) => {
-      console.log(req.body);
-      getData.crud(res, new Client(req.params.orgId, false, req.body), "new");
-      // });
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.status(500).send(err);
-    });
-});
+router.post("/", queries.createClient);
 
 // show
-router.get("/:id", (req, res) => {
-  // return single client record by id
-  getData.crud(res, new Client(req.params.orgId, req.params.id, false), "view");
-});
+router.get("/:id", queries.getClientById);
 
 // update
-router.put("/:id", (req, res) => {
-  // update single client record by id
-  getData.crud(
-    res,
-    new Client(req.params.orgId, req.params.id, req.body),
-    "edit"
-  );
-});
+router.put("/:id", queries.updateClientById);
 
 // destroy
-router.delete("/:id", (req, res) => {
-  // delete single client record by id
-  getData.crud(
-    res,
-    new Client(req.params.orgId, req.params.id, req.body),
-    "delete"
-  );
-});
+router.delete("/:id", queries.deleteClientById);
 
 module.exports = router;
