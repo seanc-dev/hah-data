0cce6578acccf3c6696462e01e4ca51b4bd8ffc6
Sean Coley <seanwcoley@gmail.com>
2020-06-16 15:28:46 +0200

    Add orgId and staffNames to data get response

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,44 @@
 const express = require("express");
 
 const ss = require("../lib/spreadsheet.js"),
-  formOptions = require("../lib/form-options.js");
+  formOptions = require("../lib/form-options.js"),
+  queries = require("./services/queries/index");
+const getData = require("./services/getData.js");
 
 const router = express.Router({
   mergeParams: true,
 });
 
 // show form
-router.get("/:orgId", function (req, res) {
-  let orgId = req.params.orgId;
-  if (orgId === "kapiti" || orgId === "wellington") {
+router.get("/:orgShortName", function (req, res) {
+  let { orgShortName } = req.params;
+  if (orgShortName === "kapiti" || orgShortName === "wellington") {
     if (!req.query.data) {
       res.render("forms", {
-        businessName: orgId,
-        businessNameDisplay: orgId[0].toUpperCase() + orgId.slice(1),
+        businessName: orgShortName,
+        businessNameDisplay:
+          orgShortName[0].toUpperCase() + orgShortName.slice(1),
       });
     } else {
-      res.send({
-        formOptions: formOptions[orgId],
-        businessName: orgId,
-        businessNameDisplay: orgId[0].toUpperCase() + orgId.slice(1),
-      });
+      console.log("orgshortname/data=true route");
+      getData
+        .getOrgDetailsByShortName(orgShortName)
+        .then(({ organisationId, staffNames }) => {
+          let data = {
+            formOptions: formOptions[orgShortName],
+            businessName: orgShortName,
+            businessNameDisplay:
+              orgShortName[0].toUpperCase() + orgShortName.slice(1),
+            organisationId,
+            staffNames,
+          };
+          console.log(data);
+          res.send(data);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send(err);
+        });
     }
   } else {
     res.status(404).send("Not found");
