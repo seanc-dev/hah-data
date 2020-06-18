de5c71fb8456e1c91aa62dcbddea7502091e7cad
Sean Coley <seanwcoley@gmail.com>
2020-06-18 20:27:30 +0200

    Rewrite getColumnHeaders to pull from getXById queries

--- routes/services/queries/index.js
+++ routes/services/queries/index.js
@@ -1,54 +1,26 @@
 const { pool } = require("./../../../lib/db_config");
+const { getJobById } = require("./job");
+const { getClientById } = require("./client");
 
 module.exports = {
   getColumnHeaders: async (tableName, orgShortName) => {
-    let columns, staffColumns;
-
-    // get columns of job table
+    const queryStr =
+      tableName === "client"
+        ? "select max(c.id) as id from client as c inner join organisation as o on c.organisationid = o.id where o.shortname = $1"
+        : "select max(j.id) as id from job as j inner join client as c on j.clientid = c.id inner join organisation as o on c.organisationid = o.id where o.shortname = $1";
     try {
-      columns = await pool.query(
-        "select column_name from information_schema.columns where table_name = $1 and column_name != 'organisationid'",
-        [tableName]
-      );
-      columns = columns.rows.map(({ column_name }) => column_name);
+      const {
+        rows: [{ id }],
+      } = await pool.query(queryStr, [orgShortName]);
+      const fn = tableName === "client" ? getClientById : getJobById;
+      const data = await fn(id, orgShortName);
+      const keys = Object.keys(data);
+      return keys.filter((key) => key !== "organisationid");
     } catch (err) {
-      console.error(err);
+      throw err;
     }
-
-    if (tableName === "job") {
-      // pull through staff member columns for job dim
-      try {
-        staffColumns = await pool.query(
-          "select 'hoursWorked'||replace(staffmembername, ' ', '') as staffHoursColumnName, 'hourlyRate'||replace(staffmembername, ' ', '') as staffRateColumnName from staff as s inner join organisation as o on s.organisationid = o.id where o.shortname = $1 order by s.id",
-          [orgShortName]
-        );
-        staffColumns = staffColumns.rows;
-      } catch (err) {
-        console.error(err);
-      }
-
-      // append staffColumns to columns
-      staffColumns.forEach((obj) => {
-        columns.push(obj.staffhourscolumnname);
-        columns.push(obj.staffratecolumnname);
-      });
-    }
-
-    return columns;
   },
-  getStaffNames: async (orgShortName) => {
-    let result;
-    try {
-      result = await pool.query(
-        "select distinct staffmembername from staff as s inner join organisation as o on s.organisationid = o.id where o.shortname = $1",
-        [orgShortName]
-      );
-    } catch (err) {
-      console.error(err);
-      return err;
-    }
-    return result.rows.map((row) => row.staffmembername);
-  },
+
   getOrgId: async (orgShortName) => {
     let result;
     try {
