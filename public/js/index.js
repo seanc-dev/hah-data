dc90834946f1c5d049d8baa9e209d42568761d1d
Sean Coley <seancoley@pm.me>
2023-03-08 15:15:15 +1300

    Formatting

--- public/js/index.js
+++ public/js/index.js
@@ -3,111 +3,116 @@ import handlers from "./handlers.js";
 import lib from "./library.js";
 
 $(document).ready(function () {
-  function init() {
-    // remove body d-none once loaded static content (ensures loader is uninterrupted)
-    document.querySelector("body").classList.remove("d-none");
+	function init() {
+		// remove body d-none once loaded static content (ensures loader is uninterrupted)
+		document.querySelector("body").classList.remove("d-none");
 
-    document.appData = {
-      businessName: document.getElementById("businessName").innerText,
-    };
+		// get orgName name from views
+		const orgName = document.getElementById("businessName").innerText;
 
-    lib
-      .initialiseAppData()
-      .then((result) => {
-        // build forms
-        buildForms();
+		console.log(document.appData);
 
-        // kill loader
-        let event = new CustomEvent("appready");
-        document.dispatchEvent(event);
-        console.log("App ready!");
-      })
-      .catch((err) => {
-        alert("There was an error loading the forms. Please refresh the page.");
-        console.error("Error in lib.initialiseAppData in index.init");
-        console.error(err);
-      });
+		document.appData = {
+			businessName: orgName,
+		};
 
-    function buildForms() {
-      // form construction
-      forms.constructForm("clientDetails");
-      forms.constructForm("jobDetails");
-      forms.setClientDetails();
-      forms.setJobDetails();
-      forms.setViewKeys();
-      forms.initAutocomplete();
+		lib
+			.initialiseAppData()
+			.then((result) => {
+				// build forms
+				buildForms();
 
-      // event handlers
-      handlers.handleFormTabClick();
-      handlers.handlerFormTypeSelect();
-      handlers.handleRecordSelectChange();
-      handlers.handleInputFocus();
-      handlers.handleAccountNameBlur();
-      handlers.handleFormSubmit();
-      handlers.handleDeleteBtnClick();
-      handlers.handleDeleteBtnConfirm();
-      handlers.handleAccountTypeInput();
-      handlers.handleAlertHide();
+				// kill loader
+				let event = new CustomEvent("appready");
+				document.dispatchEvent(event);
+				console.log("App ready!");
+			})
+			.catch((err) => {
+				alert("There was an error loading the forms. Please refresh the page.");
+				console.error("Error in lib.initialiseAppData in index.init");
+				console.error(err);
+			});
 
-      // let clientData = {
-      //     accountType: 'Business',
-      //     accountName: 'Test Test Test Ltd.',
-      //     businessName: 'Test Test Test Ltd.',
-      //     mainContactFirstName: 'Coley',
-      //     mainContactLastName: 'Coley',
-      //     mainContactLandline: '',
-      //     mainContactMobile: '+64273493710',
-      //     mainContactEmail: 'seanco.dev@gmail.com',
-      //     billingAddressStreet: '11 Island View Terrace',
-      //     billingAddressSuburb: 'Cockle Bay',
-      //     billingAddressCity: 'Auckland',
-      //     billingAddressPostcode: '2014',
-      //     territory: 'South Wellington',
-      //     customerDemographic: 'Baby Boomer (50-65 ish)',
-      //     estimatedCustomerIncome: 'Pension',
-      //     acquisitionChannel: 'Word of Mouth',
-      // }
+		function buildForms() {
+			// form construction
+			forms.constructForm(orgName, "clientDetails");
+			forms.constructForm(orgName, "jobDetails");
+			forms.setClientDetails();
+			forms.setJobDetails();
+			forms.setViewKeys();
+			forms.initAutocomplete();
 
-      // let jobData = { accountName: 'Davia, Ido',
-      // workLocationStreetAddress: '56 Kiwi Crescent',
-      // workLocationSuburb: 'Tawa',
-      // workLocationCity: '',
-      // workLocationPostcode: '',
-      // primaryJobType: 'Maintenance',
-      // secondaryJobType: '',
-      // indoorsOutdoors: 'Indoors',
-      // createdDateTimeNZT: '17/12/2019 22:12:43',
-      // dateJobEnquiry: '',
-      // dateJobQuoted: '',
-      // dateWorkCommenced: '',
-      // dateInvoiceSent: '2019-12-03',
-      // amountInvoiced: '2',
-      // costMaterials: '',
-      // costSubcontractor: '',
-      // costTipFees: '',
-      // costOther: '',
-      // hoursWorkedDave: '',
-      // workSatisfaction: '5',
-      // clientId: '10' }
+			// event handlers
+			handlers.handleFormTabClick();
+			handlers.handlerFormTypeSelect();
+			handlers.handleRecordSelectChange();
+			handlers.handleInputFocus();
+			handlers.handleAccountNameBlur();
+			handlers.handleFormSubmit();
+			handlers.handleDeleteBtnClick();
+			handlers.handleDeleteBtnConfirm();
+			handlers.handleAccountTypeInput();
+			handlers.handleAlertHide();
 
-      // applyTestData('client', clientData);
-      // applyTestData('job', jobData);
+			// let clientData = {
+			//     accountType: 'Business',
+			//     accountName: 'Test Test Test Ltd.',
+			//     businessName: 'Test Test Test Ltd.',
+			//     mainContactFirstName: 'Coley',
+			//     mainContactLastName: 'Coley',
+			//     mainContactLandline: '',
+			//     mainContactMobile: '+64273493710',
+			//     mainContactEmail: 'seanco.dev@gmail.com',
+			//     billingAddressStreet: '11 Island View Terrace',
+			//     billingAddressSuburb: 'Cockle Bay',
+			//     billingAddressCity: 'Auckland',
+			//     billingAddressPostcode: '2014',
+			//     territory: 'South Wellington',
+			//     customerDemographic: 'Baby Boomer (50-65 ish)',
+			//     estimatedCustomerIncome: 'Pension',
+			//     acquisitionChannel: 'Word of Mouth',
+			// }
 
-      // function applyTestData(dim, data){
+			// let jobData = { accountName: 'Davia, Ido',
+			// workLocationStreetAddress: '56 Kiwi Crescent',
+			// workLocationSuburb: 'Tawa',
+			// workLocationCity: '',
+			// workLocationPostcode: '',
+			// primaryJobType: 'Maintenance',
+			// secondaryJobType: '',
+			// indoorsOutdoors: 'Indoors',
+			// createdDateTimeNZT: '17/12/2019 22:12:43',
+			// dateJobEnquiry: '',
+			// dateJobQuoted: '',
+			// dateWorkCommenced: '',
+			// dateInvoiceSent: '2019-12-03',
+			// amountInvoiced: '2',
+			// costMaterials: '',
+			// costSubcontractor: '',
+			// costTipFees: '',
+			// costOther: '',
+			// hoursWorkedDave: '',
+			// workSatisfaction: '5',
+			// clientId: '10' }
 
-      //     for (let key in data) {
+			// applyTestData('client', clientData);
+			// applyTestData('job', jobData);
 
-      //         let el = document.getElementById(dim + 'Details-' + key);
+			// function applyTestData(dim, data){
 
-      //         el.value = data[key];
+			//     for (let key in data) {
 
-      //     }
+			//         let el = document.getElementById(dim + 'Details-' + key);
 
-      // }
+			//         el.value = data[key];
 
-      // applyTestData();
-    }
-  }
+			//     }
 
-  init();
+			// }
+
+			// applyTestData();
+		}
+	}
+
+	init();
 });
